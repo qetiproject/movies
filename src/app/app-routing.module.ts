8dd4b8a9f68c89c4523bfaac8c0b91647b1eee16
keti khetsuriani <k.khetsuriani@betsolutions.com>
2019-10-24 13:05:56 +0400

    ready project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: '**', component: NotFoundComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export const routingComponents = [HomeComponent, NotFoundComponent];
